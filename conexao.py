dd17a4f3406d3868240b8301379d4f6978471fbf
taijarals <taijara@outlook.com.br>
2026-04-17 20:19:53 -0300

    tela cadastro usuario

---
+++ conexao.py
@@ -0,0 +1,7 @@
+
+from supabase import create_client
+
+url = "SUA_URL_SUPABASE"
+key = "SUA_ANON_KEY"
+
+supabase = create_client(url, key)
---
+++ criar_usuario.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import conexao
+
+st.set_page_config(page_title="Cadastro de Usuário")
+
+st.title("📝 Cadastro de Usuário")
+
+with st.form("cadastro_usuario"):
+    nick = st.text_input("Nick")
+    email = st.text_input("Email")
+    senha = st.text_input("Senha", type="password")
+    tipo = st.selectbox("Tipo de Usuário", ["user", "admin", "moderador"])
+    
+    submit = st.form_submit_button("Cadastrar")
+
+if submit:
+    # validações básicas
+    if not nick or not email or not senha:
+        st.warning("Preencha todos os campos obrigatórios!")
+    
+    else:
+        try:
+            response = supabase.table("usuario").insert({
+                "nick_usuario": nick,
+                "email_usuario": email,
+                "senha_usuario": senha,
+                "tipo_usuario": tipo
+            }).execute()
+
+            if response.data:
+                st.success("Usuário cadastrado com sucesso! 🎉")
+            else:
+                st.error("Erro ao cadastrar usuário")
+
+        except Exception as e:
+            st.error(f"Erro: {e}")
