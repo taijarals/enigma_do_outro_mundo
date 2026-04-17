320912d14e872164a41b9b7be0c34146357008c5
taijarals <taijara@outlook.com.br>
2026-04-17 20:56:27 -0300

    tela login

---
+++ funcoes_login.py
@@ -0,0 +1,16 @@
+from db.supabase import supabase
+
+def login_usuario(email, senha):
+    response = supabase.table("usuario") \
+        .select("*") \
+        .eq("email_usuario", email) \
+        .eq("senha_usuario", senha) \
+        .execute()
+
+    if response.data:
+        return response.data[0]
+    return None
+
+
+def logout_usuario():
+    return None
---
+++ tela_login.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from services.auth import login_usuario
+
+def render_login():
+    st.title("🔐 Login")
+
+    with st.form("login_form"):
+        email = st.text_input("Email")
+        senha = st.text_input("Senha", type="password")
+
+        submit = st.form_submit_button("Entrar")
+
+    if submit:
+        if not email or not senha:
+            st.warning("Preencha todos os campos")
+        else:
+            user = login_usuario(email, senha)
+
+            if user:
+                st.session_state["usuario"] = user
+                st.success("Login realizado!")
+                st.rerun()
+            else:
+                st.error("Email ou senha inválidos")
