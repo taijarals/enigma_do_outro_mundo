4590ee9440ef714946be1764ef10a7fd20df08b2
taijarals <taijara@outlook.com.br>
2026-04-17 20:29:07 -0300

    chave

--- conexao.py
+++ conexao.py
@@ -1,7 +1,7 @@
 
 from supabase import create_client
 
-url = "https://ghrzodhebrxgpxkdfznn.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdocnpvZGhlYnJ4Z3B4a2Rmem5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MzM5NTUsImV4cCI6MjA4OTAwOTk1NX0.fn_PqkHWNYQGuLR6WUOqoPXXpopUi1nuVPLklfECRoo"
+url = "SUA_URL_SUPABASE"
+key = "SUA_ANON_KEY"
 
 supabase = create_client(url, key)
