6e897de3fea0a991c4da4a9484a697b5468efd71
taijarals <taijara@outlook.com.br>
2026-04-24 20:32:10 -0300

    app updates

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import streamlit as st
 from pages.login import render_login
+from pages.cadastro import render_cadastro
 from pages.home import render_home
 
 st.set_page_config(page_title="App")
 
-# inicializa sessão
+# estado inicial
 if "usuario" not in st.session_state:
     st.session_state["usuario"] = None
 
-# roteamento simples
+if "tela" not in st.session_state:
+    st.session_state["tela"] = "login"
+
+# fluxo
 if st.session_state["usuario"] is None:
-    render_login()
+
+    if st.session_state["tela"] == "login":
+        render_login()
+
+    elif st.session_state["tela"] == "cadastro":
+        render_cadastro()
+
 else:
     render_home()
